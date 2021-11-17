9ba041adbf45f316967da72ce24893bcb88718bf
Jason Phan <jason@ket.so>
2021-11-17 17:38:48 -0600

    Add fail and err functions

--- src/lib.zig
+++ src/lib.zig
@@ -16,3 +16,23 @@ pub fn requires(value: anytype, concepts: anytype) void {
         require(concept, value);
     }
 }
+
+pub fn fail(comptime concept: []const u8, comptime msg: []const u8) void {
+    const base = "concept `" ++ concept ++ "` was not satisfied";
+    const extra = " (" ++ msg ++ ")";
+
+    switch (msg.len) {
+        0 => @compileError(base),
+        else => @compileError(base ++ extra),
+    }
+}
+
+pub fn err(comptime concept: []const u8, comptime msg: []const u8) void {
+    const base = "concept `" ++ concept ++ "` could not be satisfied";
+    const extra = " (" ++ msg ++ ")";
+
+    switch (msg.len) {
+        0 => @compileError(base),
+        else => @compileError(base ++ extra),
+    }
+}

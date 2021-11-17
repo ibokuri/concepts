9f568dcb0a398217fe2d942ddd8e54743642db14
Jason Phan <jason@ket.so>
2021-11-17 09:01:04 -0600

    initial commit

---
+++ build.zig
@@ -0,0 +1,38 @@
+const std = @import("std");
+
+const Builder = std.build.Builder;
+
+const package_name = "trait";
+const package_path = "src/lib.zig";
+
+const tests = [_][]const u8{
+    //"src/tests/ser/tests.zig",
+    //"src/tests/de/tests.zig",
+};
+
+pub fn build(b: *Builder) void {
+    // Options
+    const mode = b.standardReleaseOptions();
+    const target = b.standardTargetOptions(.{});
+
+    // Tests
+    const step = b.step("test", "Run framework tests");
+
+    for (tests) |path| {
+        const t = b.addTest(path);
+
+        t.setBuildMode(mode);
+        t.setTarget(target);
+
+        t.addPackagePath(package_name, package_path);
+
+        step.dependOn(&t.step);
+    }
+
+    // Library
+    const lib = b.addStaticLibrary(package_name, package_path);
+
+    lib.setBuildMode(mode);
+    lib.setTarget(target);
+    lib.install();
+}

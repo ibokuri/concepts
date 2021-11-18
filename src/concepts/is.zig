4c54f145e60a06343847b8a4e9427bb705b40904
Jason Phan <jason@ket.so>
2021-11-17 19:44:59 -0600

    Is: Take two types instead of a value and a type

--- src/concepts/is.zig
+++ src/concepts/is.zig
@@ -2,25 +2,25 @@ const concepts = @import("../concepts.zig");
 
 const concept = "Is";
 
-pub fn is(value: anytype) void {
-    const T = @TypeOf(value);
+pub fn is(Types: anytype) void {
+    const T = @TypeOf(Types);
 
     comptime {
         if (!concepts.traits.isTuple(T)) {
             concepts.err(concept, "expected tuple, found `" ++ @typeName(T) ++ "`");
         }
 
-        if (value.len != 2) {
+        if (Types.len != 2) {
             concepts.err(concept, "expected two-tuple, found `" ++ @typeName(T) ++ "`");
         }
 
-        if (!concepts.traits.is(@TypeOf(value[1]), type)) {
-            concepts.err(concept, "expected type `type`, found `" ++ @typeName(@TypeOf(value[0])) ++ "`");
+        if (!concepts.traits.is(Types[0], type) or !concepts.trait.is(Types[1], type)) {
+            concepts.err(concept, "expected type `type`, found `" ++ @typeName(@TypeOf(Types[0])) ++ "`");
         }
     }
 
     comptime {
-        if (@TypeOf(value[0]) != value[1]) {
+        if (Types[0] != Types[1]) {
             concepts.fail(concept, "");
         }
     }

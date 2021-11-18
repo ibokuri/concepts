48f7ef694a2233e8a0eae40690ab6128fb769be1
Jason Phan <jason@ket.so>
2021-11-18 08:02:10 -0600

    Tuple: Avoid recursive calls between Is and Tuple concepts

--- src/concepts/tuple.zig
+++ src/concepts/tuple.zig
@@ -2,10 +2,16 @@ const concepts = @import("../lib.zig");
 
 const concept = "Tuple";
 
+///
+///
+/// The `Is` concept cannot be used as an invariant since it depends on the
+/// `Tuple` concept.
 pub fn tuple(T: anytype) void {
     comptime {
         // Invariants
-        concepts.is(.{ @TypeOf(T), type });
+        if (@TypeOf(T) != type) {
+            concepts.err(concept, "type `" ++ @typeName(@TypeOf(T)) ++ "` is not `type`");
+        }
 
         // Constraints
         if (!concepts.traits.isTuple(T)) {

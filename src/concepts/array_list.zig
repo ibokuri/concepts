cff81e28035ff4cb85b219fc486c124df4ff0969
Jason Phan <jason@ket.so>
2021-11-17 19:47:16 -0600

    ArrayList: Avoid StartsWith check
    This avoids an odd StartsWith trait and let's us support aligned array lists separately.

--- src/concepts/array_list.zig
+++ src/concepts/array_list.zig
@@ -3,11 +3,29 @@ const concepts = @import("../lib.zig");
 
 const concept = "ArrayList";
 
-pub fn arrayList(value: anytype) void {
-    const name = @typeName(@TypeOf(value));
+pub fn arrayList(T: anytype) void {
+    const name = @typeName(T);
 
     comptime {
-        if (!concepts.traits.startsWith(name, "std.array_list.ArrayList")) {
+        if (!concepts.traits.is(@TypeOf(T), type)) {
+            concepts.err(concept, "expected type `type`, found `" ++ name ++ "`");
+        }
+    }
+
+    comptime {
+        if (!concepts.traits.isContainer(T)) {
+            concepts.fail("ArrayList", "type `" ++ name ++ "` is not a container");
+        }
+
+        if (!concepts.traits.containsDecl("Slice", T)) {
+            concepts.fail("ArrayList", "missing `Slice` declaration");
+        }
+
+        if (!concepts.traits.isSlice(T.Slice)) {
+            concepts.fail("ArrayList", "`Slice` declaration is not a slice");
+        }
+
+        if (!concepts.traits.is(T, std.ArrayList(std.meta.Child(T.Slice)))) {
             concepts.fail("ArrayList", "`" ++ name ++ "` is not `std.ArrayList`");
         }
     }

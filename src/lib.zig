9293ca53f73228e73a03332d2bb27b3a94d76067
Jason Phan <jason@ket.so>
2021-11-19 23:00:48 -0600

    Ensure that err and fail run at comptime

--- src/lib.zig
+++ src/lib.zig
@@ -8,9 +8,11 @@ pub fn err(comptime concept: []const u8, comptime msg: []const u8) void {
     const base = "concept `" ++ concept ++ "` could not be satisfied";
     const extra = " (" ++ msg ++ ")";
 
-    switch (msg.len) {
-        0 => @compileError(base),
-        else => @compileError(base ++ extra),
+    comptime {
+        switch (msg.len) {
+            0 => @compileError(base),
+            else => @compileError(base ++ extra),
+        }
     }
 }
 
@@ -18,8 +20,10 @@ pub fn fail(comptime concept: []const u8, comptime msg: []const u8) void {
     const base = "concept `" ++ concept ++ "` was not satisfied";
     const extra = " (" ++ msg ++ ")";
 
-    switch (msg.len) {
-        0 => @compileError(base),
-        else => @compileError(base ++ extra),
+    comptime {
+        switch (msg.len) {
+            0 => @compileError(base),
+            else => @compileError(base ++ extra),
+        }
     }
 }
